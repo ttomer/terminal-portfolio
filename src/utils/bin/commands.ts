3ef639bf1d2d7a456ebf8e2ebc0ab835cbdafbfa
Tomer Torgeman <tomerswazi@gmail.com>
2024-11-10 14:02:53 -0800

    Update theme

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -167,6 +167,7 @@ export const banner = (args?: string[]): string => {
 (_________________)
 
 Type 'help' to see the list of available commands.
+Type 'portfolio' - my portfolio.
 Type 'summary' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
