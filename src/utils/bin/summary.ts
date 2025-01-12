7f0ab8644f218cb641574b26df81f3e11f378f04
Tomer Torgeman <tomerswazi@gmail.com>
2025-01-12 15:19:25 -0800

    Portfolio correction

--- src/utils/bin/summary.ts
+++ src/utils/bin/summary.ts
@@ -7,7 +7,7 @@ const summary = async (args: string[]): Promise<string> => {
         @@@@               @@@@             -----------
       @@                       @@            ABOUT
     @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
+  @@                               @@        <u><a href="${config.portfolio}" target="_blank">portfolio</a></u>
  @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
 @@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 @@                                   @@     -----------
